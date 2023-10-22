cc5c1b1c66fcfa079f128429e0553b6e922c9680
CaptinJac <bcochajil@gmail.com>
2023-10-21 22:35:09 -0600

    Added Hit func

--- test.py
+++ test.py
@@ -36,19 +36,39 @@ def deal(deck):
         deck.pop(0)
     return hand
 
+def hit (hand,shoe):
+    hand.append(shoe[0])
+    shoe.pop(0)
+    hand_value = []
+    for x in hand:
+        hand_value.append(x.value)
+    while sum(hand_value) < 21:
+        player_input = input("Your current total is %s. Would you like to hit (h) or stay (s)?\n"% sum(hand_value)) 
+        if player_input.lower() == "h":
+            hand.append(shoe[0])
+            hand_value.append(hand[len(hand) - 1])
+            shoe.pop(0)
+        elif player_input.lower() ==  "s":
+            break
+
+
 deck = build_deck()
 shuffle(deck)
 
 player_hand = deal(deck)
 dealer_hand = deal(deck)
 
-for x in player_hand:
-    print(x)
-
-for x in dealer_hand:
-    print(x)
-
 change_value(player_hand)
 change_value(dealer_hand)
 
-win_condition(player_hand, dealer_hand)
+hand_value = []
+for x in player_hand:
+    hand_value.append(x.value)
+
+player_input = input("Your current hand is %s, Would you like to Hit (h)? \n" %sum(hand_value))
+
+if player_input.lower() == "h":
+    hit(player_hand, deck)
+    win_condition(player_hand, dealer_hand)
+else:
+    win_condition(player_hand, dealer_hand)
